e17930a01da067ea177a83328090b734c73de6c9
Hrtsuna24 <149830352+hrtsnmi@users.noreply.github.com>
2024-02-26 23:02:35 +0200

    InitProject

---
+++ Source/FPTProject/FPTProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPTProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPTProject, "FPTProject" );
+ 
